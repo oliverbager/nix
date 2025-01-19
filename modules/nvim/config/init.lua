75d1baa7c9efaad3152adcd8cc5371ca2528e852
Oliver <oliver.nielsen.bager@gmail.com>
2025-01-19 06:33:35 +0100

    LuaSnip

--- modules/nvim/config/init.lua
+++ modules/nvim/config/init.lua
@@ -1,2 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd[[
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+]]
+
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+
+require("luasnip").config.set_config({ -- Setting LuaSnip config
+
+  enable_autosnippets = true,
+  store_selection_keys = "<Tab>",
+})
