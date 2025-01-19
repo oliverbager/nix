108a2e1d92ac130468a15e9e1e678e82803c21b8
Oliver <oliver.nielsen.bager@gmail.com>
2025-01-19 11:05:26 +0100

    auto wrap lines

--- modules/nvim/config/lua/config/options.lua
+++ modules/nvim/config/lua/config/options.lua
@@ -2,12 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.maplocalleader = ","
-vim.g.vimtex_quickfix_ignore_filters = [
-  \ 'Missing',
-  \ 'Undefined',
-  \ 'Underfull',
-  \ 'Overfull',
-  \ 'Warning',
-  \]
 vim.opt.wrap = true
