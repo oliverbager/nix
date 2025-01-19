4dbf59b8efa5235af944d1b9b890d7d014e662d2
Oliver <oliver.nielsen.bager@gmail.com>
2025-01-19 10:05:22 +0100

    snippets

--- modules/nvim/config/LuaSnip/tex.lua
+++ modules/nvim/config/LuaSnip/tex.lua
@@ -3,20 +3,20 @@ local in_mathzone = function()
 end
 
 return {
-  s({trig = "env", snippetType = "autosnippet"},
-    fmta(
-      [[
-      \begin{<>}
-          <>
-      \end{<>}
-      ]],
-      {
-        i(1),
-        i(2),
-        rep(1),
-      }
-    )
-  ),
+  -- s({trig = "env", snippetType = "autosnippet"},
+  --   fmta(
+  --     [[
+  --     \begin{<>}
+  --         <>
+  --     \end{<>}
+  --     ]],
+  --     {
+  --       i(1),
+  --       i(2),
+  --       rep(1),
+  --     }
+  --   )
+  -- ),
 
   s({trig = "ff", snippetType = "autosnippet"},
     fmta(
