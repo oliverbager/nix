4a48aea59cc5d9031582052c8ea974777343eb3b
Oliver <oliver.nielsen.bager@gmail.com>
2025-01-19 10:47:05 +0100

    nvim options + latex warnings

--- modules/nvim/config/lua/config/options.lua
+++ modules/nvim/config/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.maplocalleader = ","
+vim.g.vimtex_quickfix_ignore_filters = [
+  \ 'Missing',
+  \ 'Undefined',
+  \ 'Underfull',
+  \ 'Overfull',
+  \ 'Warning',
+  \]
+vim.opt.wrap = true
