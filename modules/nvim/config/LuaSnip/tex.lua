753ca63345bd9daf406e6babc30af9e1a5e9b7e5
Oliver <oliver.nielsen.bager@gmail.com>
2025-01-19 07:09:37 +0100

    snippets

--- modules/nvim/config/LuaSnip/tex.lua
+++ modules/nvim/config/LuaSnip/tex.lua
@@ -3,6 +3,21 @@ local in_mathzone = function()
 end
 
 return {
+s({trig = "env", snippetType = "autosnippet"},
+  fmta(
+    [[
+      \\begin{<>}
+        <>
+      \\end{<>}
+    ]],
+    {
+      i(1),
+      i(2),
+      rep(1),
+    }
+  )
+),
+
 s({trig = "ff", snippetType = "autosnippet"},
   fmta(
     "\\frac{<>}{<>}",
@@ -13,4 +28,121 @@ s({trig = "ff", snippetType = "autosnippet"},
   ),
   {condition = in_mathzone}
 ),
+
+s({trig = "bmat", snippetType = "autosnippet"},
+  fmta(
+    "\\begin{bmatrix}<>\\end{bmatrix}",
+    {
+      i(1),
+      i(2),
+    }
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "^", snippetType = "autosnippet"},
+  fmta(
+    "^{<>}",
+    {
+      i(1),
+    }
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "_", snippetType = "autosnippet"},
+  fmta(
+    "_{<>}",
+    {
+      i(1),
+    }
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "sq", snippetType = "autosnippet"},
+  fmta(
+    "\\sqrt{<>}",
+    {
+      i(1),
+    }
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "tt", snippetType = "autosnippet"},
+  fmta(
+    "\\text{<>}",
+    {
+      i(1),
+    }
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "ldd", snippetType = "autosnippet"},
+  fmta(
+    "\\ldots",
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "cdd", snippetType = "autosnippet"},
+  fmta(
+    "\\cdots",
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "sum", snippetType = "autosnippet"},
+  fmta(
+    "\\sum_{k=<>}^{<>}",
+    {
+      i(1),
+      i(2),
+    }
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "in1", snippetType = "autosnippet"},
+  fmta(
+    "\\int_{<>}^{<>}",
+    {
+      i(1),
+      i(2),
+    }
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "in2", snippetType = "autosnippet"},
+  fmta(
+    "\\iint_{<>}^{<>}",
+    {
+      i(1),
+      i(2),
+    }
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "in3", snippetType = "autosnippet"},
+  fmta(
+    "\\iiint_{<>}^{<>}",
+    {
+      i(1),
+      i(2),
+    }
+  ),
+  {condition = in_mathzone}
+),
+
+s({trig = "=>", snippetType = "autosnippet"},
+  fmta(
+    "\\implies",
+  ),
+  {condition = in_mathzone}
+),
+
 }
