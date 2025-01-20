ebeb4fe351e4ac2424d75645c143fc3909c88cda
Oliver <oliver.nielsen.bager@gmail.com>
2025-01-20 06:29:25 +0100

    change file names

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.oliver = import ./home.nix;
+            home-manager.users.oliver = import ./desktop-home.nix;
           }
         ];
       };
